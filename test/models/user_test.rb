c4aad2864f49b3f357ac4bd93dd0f9cff347a11d
Sheila Leveille <sbleveille@gmail.com>
2020-09-17 19:15:57 -0500

    FEAT: added new tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#full_name method returns capitalized first and last names" do
+    user = User.new(first_name: "sheila", last_name: "leveille")
+    assert_equal "Sheila Leveille", user.full_name
+  end
 end
